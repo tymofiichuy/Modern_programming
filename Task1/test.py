d59a8a6dd42aebf03f36743e423be39f213232d0
tymofiichuy <142347014+tymofiichuy@users.noreply.github.com>
2024-03-01 19:04:14 +0200

    Rename tests in test.py

--- Task1/test.py
+++ Task1/test.py
@@ -1,11 +1,11 @@
 from function import *
 
 
-def legendre_quad_res_test():
+def test_legendre_quad_res():
     assert legendre(24, 43) == 1
 
-def legendre_quad_non_res_test():
+def test_legendre_quad_non_res():
     assert legendre(11, 17) == -1
 
-def legendre_div_test():
+def test_legendre_div():
     assert legendre(4, 16) == 0
