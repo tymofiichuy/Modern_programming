ee180d091793c1c3a3333033602dc183c29502da
tymofiichuy <142347014+tymofiichuy@users.noreply.github.com>
2024-04-11 13:44:55 +0300

    Update function

--- Task2/file_search/file_search.cpp
+++ Task2/file_search/file_search.cpp
@@ -1,34 +1,45 @@
 ﻿#include <iostream>
 #include <windows.h>
-#include <vector>
+#include <chrono>
 using namespace std;
+
+void name_check(WIN32_FIND_DATA data, wstring directory, wstring file) {
+	if (wcscmp(data.cFileName, file.c_str()) == 0) {
+		wcout << directory << L"/" << data.cFileName << endl;
+	}
+}
 
 void file_search(wstring directory, wstring file) {
 	WIN32_FIND_DATA data;
 	HANDLE hfind;
 
-
 	hfind = FindFirstFileW((directory + L"/*").c_str(), &data);
 	if (hfind == INVALID_HANDLE_VALUE) {
-		//>> stderr
+		//>> cerr
 	}
 	else {
-		if (data.dwFileAttributes == FILE_ATTRIBUTE_DIRECTORY) {
-			if (wcscmp(data.cFileName, L".") != 0 && wcscmp(data.cFileName, L"..") != 0) {
-				file_search(directory + L"/" + data.cFileName, file);
-			}
+		if (data.dwFileAttributes == FILE_ATTRIBUTE_DIRECTORY 
+			&& wcscmp(data.cFileName, L".") != 0 && wcscmp(data.cFileName, L"..") != 0) {
+			file_search(directory + L"/" + data.cFileName, file);
 		}
 		else {
-			if (wcscmp(data.cFileName, file.c_str()) == 0) {
-				wcout << directory << L"/" << data.cFileName << endl;
-			}
+			name_check(data, directory, file);
 		}
 		while (FindNextFileW(hfind, &data) != 0) {
-			wcout << directory << L"/" << data.cFileName << endl;
+			if (data.dwFileAttributes == FILE_ATTRIBUTE_DIRECTORY
+				&& wcscmp(data.cFileName, L".") != 0 && wcscmp(data.cFileName, L"..") != 0) {
+				file_search(directory + L"/" + data.cFileName, file);
+			}
+			else {
+				name_check(data, directory, file);
+			}
 		}
 		FindClose(hfind);
 	}
 }
-int main(int argc, char* argv[]) {
-	//Some code
+int main() {
+	auto begin = std::chrono::high_resolution_clock::now();
+	//file_search(path , file);
+	auto end = std::chrono::high_resolution_clock::now();
+	wcout << L"\nSerch time:\n" << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << L" ms";
 }
