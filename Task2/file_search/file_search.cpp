a266f559870fc41ba0113c40780ca073dd1f9d50
tymofiichuy <142347014+tymofiichuy@users.noreply.github.com>
2024-04-13 16:27:42 +0300

    Add files via upload

--- Task2/file_search/file_search.cpp
+++ Task2/file_search/file_search.cpp
@@ -3,43 +3,81 @@
 #include <chrono>
 using namespace std;
 
-void name_check(WIN32_FIND_DATA data, wstring directory, wstring file) {
-	if (wcscmp(data.cFileName, file.c_str()) == 0) {
-		wcout << directory << L"/" << data.cFileName << endl;
+//to do
+//1. input processing +
+//2. error handling +
+//3. basic tests, setting enviroment to run
+//4. handling cyrillic symbols, additional tests -
+
+void name_check(WIN32_FIND_DATAA data, string file, string directory) {
+	if (strcmp(data.cFileName, file.c_str()) == 0) {
+		cout << directory << "/" << data.cFileName << endl;
 	}
 }
 
-void file_search(wstring directory, wstring file) {
-	WIN32_FIND_DATA data;
+void file_search(string file, string directory) {
+	WIN32_FIND_DATAA data;
 	HANDLE hfind;
 
-	hfind = FindFirstFileW((directory + L"/*").c_str(), &data);
+	hfind = FindFirstFileA((directory + "/*").c_str(), &data);
 	if (hfind == INVALID_HANDLE_VALUE) {
-		//>> cerr
-	}
-	else {
-		if (data.dwFileAttributes == FILE_ATTRIBUTE_DIRECTORY 
-			&& wcscmp(data.cFileName, L".") != 0 && wcscmp(data.cFileName, L"..") != 0) {
-			file_search(directory + L"/" + data.cFileName, file);
+		if (GetLastError() == ERROR_ACCESS_DENIED) {
+			return;
 		}
 		else {
-			name_check(data, directory, file);
+			throw runtime_error("Directory do not exist.");
 		}
-		while (FindNextFileW(hfind, &data) != 0) {
+	}
+	else {
+		while (FindNextFileA(hfind, &data) != 0) {
 			if (data.dwFileAttributes == FILE_ATTRIBUTE_DIRECTORY
-				&& wcscmp(data.cFileName, L".") != 0 && wcscmp(data.cFileName, L"..") != 0) {
-				file_search(directory + L"/" + data.cFileName, file);
+				&& strcmp(data.cFileName, ".") != 0 && strcmp(data.cFileName, "..") != 0) {
+				file_search(file, directory + "/" + data.cFileName);
 			}
 			else {
-				name_check(data, directory, file);
+				name_check(data, file, directory);
 			}
 		}
 		FindClose(hfind);
 	}
 }
-int main() {
-	auto begin = std::chrono::high_resolution_clock::now();
-	//file_search(path , file);
-	auto end = std::chrono::high_resolution_clock::now();
-	wcout << L"\nSerch time:\n" << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << L" ms";
+
+int main(int argc, char** argv) {
+	if (argc != 3 and argc != 2) {
+		cerr << "Argument do not correspond signature.";
+		return 1;
+	}
+	else if (argc == 2) {
+		DWORD drives = GetLogicalDrives();
+		if (drives == 0) {
+			cerr << "Drives search failed. Error: " << GetLastError;
+			return 1;
+		}
+		else {
+			string file = string(argv[1], argv[1] + strlen(argv[1]));
+			for (int i = 0; i < 26; i++) {
+				if (drives & (1 << i)) {
+					string drive(1, 'A' + i);
+					file_search(file, drive + ":");
+				}
+			}
+		}
+	}
+	else {
+		string file = string(argv[1], argv[1] + strlen(argv[1]));
+		string directory = string(argv[2], argv[2] + strlen(argv[2]));
+		try {
+			file_search(file, directory);
+		}
+		catch (runtime_error& e) {
+			cerr << e.what();
+			return 1;
+		}
+	}
+	return 0;
 }
+
+//int main() {
+//	file_search("Example-1.txt", "C:");
+//	return 0;
+//}
