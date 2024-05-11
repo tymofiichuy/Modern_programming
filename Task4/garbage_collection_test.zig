ecf36482c228325a1d25ee57b81e2a20618de3f6
tymofiichuy <142347014+tymofiichuy@users.noreply.github.com>
2024-05-11 23:40:39 +0300

    Update garbage_collection_test.zig

--- Task4/garbage_collection_test.zig
+++ Task4/garbage_collection_test.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 const allocator = std.testing.allocator;
 
+//Test creates memory leak and detects it,
+//so there are no garbage collector
 test "memory_leak" {
     var list = std.ArrayList(u8).init(allocator);
     try list.append(0);
