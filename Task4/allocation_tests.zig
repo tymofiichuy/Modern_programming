caf049786968eb62e1db20369439601e4e2a4ff8
tymofiichuy <tymofiichuy@gmail.com>
2024-05-12 13:55:03 +0300

    Change allocation tests

--- Task4/allocation_tests.zig
+++ Task4/allocation_tests.zig
@@ -2,27 +2,21 @@ const std = @import("std");
 var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 const allocator = gpa.allocator();
 
-fn alloc_var_on_stack() void {
-    while (true) {
-        var int = 0;
-        int = int + 1;
-    }
+fn stack_overflow() void {
+    stack_overflow();
 }
 
 fn alloc_var_on_heap() void {
     while (true) {
-        const ptr = try allocator.alloc(u8, 1);
+        const ptr = try allocator.alloc(u8, 1024);
         ptr[0] = 1;
     }
 }
 
-//Function infinitely allocates memory on stack, but due to variable lifetime this
-//can't cause overflow. This test should fail
 test "stack_allocation_test" {
-    try std.testing.expectError(error.OutOfMemory, alloc_var_on_stack());
+    try std.testing.expectError(error.OutOfMemory, stack_overflow());
 }
 
-//Function infinitely allocates memory on heap, which will run out memory
 test "heap_allocation_test" {
     try std.testing.expectError(error.OutOfMemory, alloc_var_on_heap());
 }
