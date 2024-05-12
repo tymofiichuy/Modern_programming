915d601f455661236e5f913adaa955e3a492c311
tymofiichuy <142347014+tymofiichuy@users.noreply.github.com>
2024-05-12 16:46:36 +0300

    Update allocation_tests.zig

--- Task4/allocation_tests.zig
+++ Task4/allocation_tests.zig
@@ -6,17 +6,17 @@ fn stack_overflow() !void {
     try stack_overflow();
 }
 
-fn alloc_var_on_heap() !void {
-    while (true) {
-        const ptr = try allocator.alloc(u8, 1024);
-        ptr[0] = 1;
-    }
-}
+//fn alloc_var_on_heap() !void {
+//    while (true) {
+//        const ptr = try allocator.alloc(u8, 1024);
+//        ptr[0] = 1;
+//    }
+//}
 
 test "stack_allocation_test" {
     std.testing.expectError(error.OutOfMemory, stack_overflow());
 }
 
-test "heap_allocation_test" {
-    std.testing.expectError(error.OutOfMemory, alloc_var_on_heap());
-}
+//test "heap_allocation_test" {
+//    std.testing.expectError(error.OutOfMemory, alloc_var_on_heap());
+//}
