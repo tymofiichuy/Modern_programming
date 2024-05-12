73db51bde4aa0451e41b3711cfc0d5c276eef864
tymofiichuy <142347014+tymofiichuy@users.noreply.github.com>
2024-05-12 19:42:40 +0300

    Update allocation_tests.zig

--- Task4/allocation_tests.zig
+++ Task4/allocation_tests.zig
@@ -1,13 +1,13 @@
 const std = @import("std");
 //var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 //const allocator = gpa.allocator();
-const allocator = std.mem.Allocator;
+const Allocator = std.mem.Allocator;
 
 //fn stack_overflow() !void {
 //    try stack_overflow();
 //}
 
-fn alloc_var_on_heap() !void {
+fn alloc_var_on_heap(allocator: *Allocator) !void {
     while (true) {
         const ptr = try allocator.alloc(u8, 1024);
         ptr[0] = 1;
